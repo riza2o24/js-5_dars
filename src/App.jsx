0208e3a1f351da33a2a289abe19f7c71bb219e6b
Otabek <abdullayevriza80@gmail.com>
2026-04-27 22:10:39 +0500

    wert

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import logo from "../../5-darss/src/assets/logo.png"
+import Header from "./components/header/Header"
+
+const nav_menu =[
+  {
+    title: "About",
+    link: "/about"
+  },
+  {
+    title: "Features",
+    link: "/features"
+  },
+  {
+    title: "Pricing",
+    link: "/pricing"
+  },
+  {
+    title: "Testimonials",
+    link: "/testimonials"
+  },
+  {
+    title: "Help",
+    link: "/help"
+  }
+]
+
+const btns = ["Sign In ", "Sign up"]
+
+function App() {
+
+  return (
+    <>
+      <Header logo={logo} navLink={nav_menu} nav_btns={btns} />
+    </>
+  )
+}
+
+export default App
